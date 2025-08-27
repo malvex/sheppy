8b7d5c12bcfc28f2924dce2093e80f78e0bb5ec0
Martin <15851033+malvex@users.noreply.github.com>
2025-08-27 08:17:56 +0200

    feat(cli): add auto-reload support

--- src/sheppy/cli/commands/work.py
+++ src/sheppy/cli/commands/work.py
@@ -5,8 +5,9 @@ import sys
 
 import typer
 from rich.logging import RichHandler
+from watchfiles import run_process
 
-from sheppy import Worker
+from sheppy import Backend, Worker
 
 from ..utils import BackendType, LogLevel, console, get_backend
 
@@ -16,6 +17,7 @@ def work(
     backend: BackendType = typer.Option(BackendType.redis, "--backend", "-b", help="Queue backend type"),
     redis_url: str = typer.Option("redis://127.0.0.1:6379", "--redis-url", "-r", help="Redis server URL"),
     max_concurrent: int = typer.Option(10, "--max-concurrent", "-c", help="Max concurrent tasks", min=1),
+    autoreload: bool = typer.Option(False, "--reload", help="Reload worker on file changes"),
     log_level: LogLevel = typer.Option(LogLevel.info, "--log-level", "-l", help="Logging level"),
 ) -> None:
     """Start a worker to process tasks from a queue."""
@@ -24,17 +26,6 @@ def work(
     cwd = os.getcwd()
     if cwd not in sys.path:
         sys.path.insert(0, cwd)
-
-    worker_logger = logging.getLogger("sheppy.worker")
-
-    if not worker_logger.hasHandlers():
-        worker_logger.setLevel(log_level.to_logging_level())
-        worker_logger.addHandler(RichHandler(
-            rich_tracebacks=True,
-            tracebacks_show_locals=True,
-            log_time_format="[%X] ",
-            show_path=False
-        ))
 
     backend_instance = get_backend(backend, redis_url)
 
@@ -47,6 +38,26 @@ def work(
     console.print(f"  Max concurrent tasks: [yellow]{max_concurrent}[/yellow]")
     console.print()
 
-    worker = Worker(queue, backend=backend_instance, max_concurrent_tasks=max_concurrent)
+    if autoreload:
+        run_process('.', target=_start_worker,
+                    args=(queue, backend_instance, max_concurrent, log_level),
+                    callback=lambda _: console.print("Detected file changes, reloading worker..."))
+    else:
+        _start_worker(queue, backend_instance, max_concurrent, log_level)
 
+
+def _start_worker(queue: str, backend: Backend, max_concurrent: int, log_level: LogLevel) -> None:
+
+    worker_logger = logging.getLogger("sheppy.worker")
+
+    if not worker_logger.hasHandlers():
+        worker_logger.setLevel(log_level.to_logging_level())
+        worker_logger.addHandler(RichHandler(
+            rich_tracebacks=True,
+            tracebacks_show_locals=True,
+            log_time_format="[%X] ",
+            show_path=False
+        ))
+
+    worker = Worker(queue, backend=backend, max_concurrent_tasks=max_concurrent)
     asyncio.run(worker.work())
