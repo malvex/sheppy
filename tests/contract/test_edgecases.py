b7709db136be74f4173e4acb1aa758fed1298227
Martin <15851033+malvex@users.noreply.github.com>
2025-11-18 03:10:19 +0100

    fix: Task exception was never retrieved if return annotation validation fails

---
+++ tests/contract/test_edgecases.py
@@ -0,0 +1,18 @@
+from sheppy import Queue, Worker, task
+from sheppy.testqueue import assert_is_failed
+
+
+@task
+async def task_with_wrong_return_annotation() -> list:
+    return 5
+
+
+async def test_wrong_return_annotation(queue: Queue, worker: Worker) -> None:
+    t = task_with_wrong_return_annotation()
+
+    await queue.add(t)
+    await worker.work(1)
+
+    t = await queue.get_task(t)
+
+    assert_is_failed(t)
